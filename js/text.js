a3a7c8693fd85c56a8e5ad5efc0cfa3110158a12
xasan <xasan>
2025-03-25 15:53:19 +0500

    solutions media

--- js/text.js
+++ js/text.js
@@ -1,27 +1,35 @@
-const words = ["CISCO CCNA", "Google", "QA-инженер", "Java разработчик","Huawei HCIA","Windows Server"];
-let index = 0;
-let charIndex = 0;
-const span = document.querySelector("#text-anim span:first-child");
-let isDeleting = false;
+document.addEventListener("DOMContentLoaded", () => {
+    const words = ["CISCO CCNA", "Java", "Google", "Huawei HCIA", "QA-инженер", "Администратор", "Windows Server"];
+    let index = 0;
+    let charIndex = 0;
+    let isDeleting = false;
 
-function typeEffect() {
-    let currentWord = words[index];
-    if (isDeleting) {
-        charIndex--;
-    } else {
-        charIndex++;
+    const span = document.querySelector("#text__changer");
+
+    if (!span) {
+        console.error("Element #text__changer topilmadi!");
+        return;
     }
 
-    span.textContent = currentWord.substring(0, charIndex);
+    function typeEffect() {
+        let currentWord = words[index];
+        if (isDeleting) {
+            charIndex--;
+        } else {
+            charIndex++;
+        }
 
-    if (!isDeleting && charIndex === currentWord.length) {
-        setTimeout(() => isDeleting = true, 1000); // To‘liq yozilganda 1 sek kutadi
-    } else if (isDeleting && charIndex === 0) {
-        isDeleting = false;
-        index = (index + 1) % words.length; // Keyingi so‘zga o‘tadi
+        span.textContent = currentWord.substring(0, charIndex);
+
+        if (!isDeleting && charIndex === currentWord.length) {
+            setTimeout(() => isDeleting = true, 1000);
+        } else if (isDeleting && charIndex === 0) {
+            isDeleting = false;
+            index = (index + 1) % words.length;
+        }
+
+        setTimeout(typeEffect, isDeleting ? 50 : 100);
     }
 
-    setTimeout(typeEffect, isDeleting ? 50 : 100); // Tezlikni moslaydi
-}
-
-typeEffect();
+    typeEffect();
+});
